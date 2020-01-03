2d979373c476649a2a19c909567647b810d0afc1
Michael Bahr <michael.bahr@live.de>
2020-01-03 20:10:31 +0100

    Added sorting and more logging

--- tagger.py
+++ tagger.py
@@ -14,18 +14,20 @@ args = parser.parse_args()
 
 region = args.region
 target_tags = args.tags.split(",")
+target_tags.sort()
 
 client = boto3.client('lambda', region)
 
-print(f"Loading functions for region {region}")
+print(f"Loading functions for region '{region}'")
 functions = client.list_functions().get('Functions', [])
+functions.sort(key=lambda f: f['FunctionArn'])
 
 if args.write:
     with open(args.file) as file:
         tags_config = yaml.load(file)
         for target_tag in target_tags:
             if target_tag not in tags_config:
-                print(f"{target_tag} is missing from the yaml file. Please add it to the file or omit the tag. Aborting.")
+                print(f"'{target_tag}' is missing from the yaml file. Please add it to the file or omit the tag. Aborting.")
                 exit()
 
 for function in functions:
@@ -38,25 +40,29 @@ for function in functions:
         print(f"Processing tag '{target_tag}'.")
         if target_tag not in function_tags or args.overwrite:
             if not args.overwrite:
-                print(f"Function {function['FunctionArn']} is missing tag '{target_tag}'")
+                print(f"Function {function['FunctionArn']} is missing tag '{target_tag}'.")
 
             if args.write:
                 new_tags = {}
                 if target_tag in tags_config and tags_config[target_tag] is not None:
                     for tag_value, arn_parts in tags_config[target_tag].items():
+                        if arn_parts is None:
+                            print(f"The tag '{target_tag}'s value {tag_value} has no arn_parts in the yaml file.")
+                            continue
+                        arn_parts.sort()
                         for arn_part in arn_parts:
                             if arn_part in function['FunctionArn']:
                                 new_tags[target_tag] = tag_value
                 else:
-                    print(f"Tag {target_tag} has no values in the yaml file.")
+                    print(f"Tag '{target_tag}' has no values in the yaml file.")
 
                 if len(new_tags) > 0:
                     print(f"Adding tags to function {function['FunctionArn']}: {new_tags}")
                     if not args.dry:
                         client.tag_resource(Resource=function['FunctionArn'], Tags=new_tags)
                 else:
-                    print(f"No new tags for function {function['FunctionArn']}")
+                    print(f"No new tags for function {function['FunctionArn']}.")
             else:
                 print("Write is disabled. Tags are not updated. Use --write to activate it. Use --write AND --dry-run for a dry run.")
         else:
-            print(f"Function already has the tag {target_tag}.")
+            print(f"Function already has the tag '{target_tag}'.")
